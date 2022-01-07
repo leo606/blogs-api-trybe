daa033c1e1d9aefbdf9888f729fc12ac3e7f8469
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-01-07 09:58:19 -0300

    create blogposts model

---
+++ models/blogposts.js
@@ -0,0 +1,13 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPosts = sequelize.define(
+    'BlogPosts',
+    {
+      title: DataTypes.STRING,
+      content: DataTypes.STRING,
+      userId: DataTypes.INTEGER,
+      published: DataTypes.DATE,
+      updated: DataTypes.DATE,
+    },
+    );
+  return BlogPosts;
+};
