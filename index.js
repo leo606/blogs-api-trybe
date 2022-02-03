2e6996a03ed0cdc98d18e50d8efcd4e7479250d5
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-02-03 10:19:24 -0300

    remove comment

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-// não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
